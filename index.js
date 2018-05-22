11d7e37032d2c949b722ee98d2f47320e3b543a4
pujie <puji@gmail.com>
2018-05-22 16:35:00 +0700

    add statusupdater on visits

--- index.js
+++ index.js
@@ -19,4 +19,18 @@ app.get('/askotp/:msg',function(req,res){
         res.send("Email telah terkirim : "+content);
     });
 });
+app.get('/checkotp/:otp',function(req,res){
+    res.header("Access-Control-Allow-Origin","*");
+    var otp = req.params.otp;
+    con.getdata(query.checkOtp(otp),function(result){
+        res.send(result);
+    });
+});
+app.get('/confirmotp/:otp',function(req,res){
+    res.header("Access-Control-Allow-Origin","*");
+    var otp = req.params.otp;
+    con.getdata(query.confirmOtp(otp),function(result){
+        res.send(result);
+    })
+})
 app.listen(process.env.PORT || 1946);
--- js/queries.js
+++ js/queries.js
@@ -1,4 +1,12 @@
-var visits = 'select createuser sales,clientname,address from visits ';
+var visits = 'select createuser sales,clientname,address from visits ',
+    checkOtp = (otp)=>{
+        return 'select * from visits where otp="'+otp+'"'
+    };
+    confirmOtp = (otp)=>{
+        return 'update visits set otpconfirmed="1" where otp="'+otp+'"'
+    }
 module.exports = {
-    visits: visits
+    visits: visits,
+    checkOtp: checkOtp,
+    confirmOtp: confirmOtp
 }
