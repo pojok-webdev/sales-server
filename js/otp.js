9b914f8318d78622391bb4990dccdf144f5f0959
pujie <puji@gmail.com>
2018-05-23 16:00:35 +0700

    add otp generator

--- js/otp.js
+++ js/otp.js
@@ -1,5 +1,5 @@
 var random4digit = ()=>{
-        return shuffle("0123456789".split('').join('').substring(0,4));
+        return shuffle("0123456789".split('')).join('').substring(0,4);
     },
     shuffle = (o)=>{
         for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
